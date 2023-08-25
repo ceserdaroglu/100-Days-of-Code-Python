db28868cf8ce71063f580cedabe52112b77534d8
Ceren Serdaroğlu <ceserdaroglu@gmail.com>
2023-08-25 04:00:16 +0300

    day 3

--- python/day_03_love_calculator.py
+++ python/day_03_love_calculator.py
@@ -1,10 +1,7 @@
-# 🚨 Don't change the code below 👇
 print("Welcome to the Love Calculator!")
 name1 = input("What is your name? \n")
 name2 = input("What is their name? \n")
-# 🚨 Don't change the code above 👆
 
-#Write your code below this line 👇
 new_name= name1.lower()+name2.lower()
 # new_name_1=str(name1.count("t")+name1.count("r")+name1.count("u")+name1.count("e")+name2.count("t")+name2.count("r")+name2.count("u")+name2.count("e"))
 # new_name_2=str(name1.count("l")+name1.count("o")+name1.count("v")+name1.count("e")+name2.count("l")+name2.count("o")+name2.count("v")+name2.count("e"))
